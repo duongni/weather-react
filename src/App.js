671c2736cea7aafdb57f20114b6427d318432340
duongni <133175915+duongni@users.noreply.github.com>
2023-06-22 23:17:23 -0400

    Added initial html structure for weather react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="search" placeholder="enter a city" />
+      </form>
+      <div className="city-weather">
+        <h2>New York</h2>
+        <h1>60°F</h1>
+        <h3>Clounds</h3>
+      </div>
+      <div className="weather-details">
+        <div>
+          <h4>34°F</h4>
+          <p>Feels Like</p>
+        </div>
+        <div>
+          <h4>70%</h4>
+          <p>Humidity</p>
+        </div>
+        <div>
+          <h4>2MPH</h4>
+          <p>Wind Speed</p>
+        </div>
+        <div className="footer">Open-source Code by Ni D.</div>
+      </div>
     </div>
   );
 }
