bec43e95f32d870a91dec239c7720bd2a4f01cf1
duongni <133175915+duongni@users.noreply.github.com>
2023-06-23 00:31:54 -0400

    Added styling to the app

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,28 +1,32 @@
 import React from "react";
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
   return (
     <div>
-      <form>
+      <form className="search">
         <input type="search" placeholder="enter a city" />
       </form>
-      <div className="city-weather">
-        <h2>New York</h2>
-        <h1>60°F</h1>
-        <h3>Clounds</h3>
-      </div>
-      <div className="weather-details">
-        <div>
-          <h4>34°F</h4>
-          <p>Feels Like</p>
+      <div className="container">
+        <div className="city-weather">
+          <h2>New York</h2>
+          <h1>60°F</h1>
+          <img src="" alt="weather icon" />
+          <h3>Clouds</h3>
         </div>
-        <div>
-          <h4>70%</h4>
-          <p>Humidity</p>
-        </div>
-        <div>
-          <h4>2MPH</h4>
-          <p>Wind Speed</p>
+        <div className="weather-details">
+          <div>
+            <h4>34°F</h4>
+            <p>Feels Like</p>
+          </div>
+          <div>
+            <h4>70%</h4>
+            <p>Humidity</p>
+          </div>
+          <div>
+            <h4>2MPH</h4>
+            <p>Wind Speed</p>
+          </div>
         </div>
       </div>
     </div>
