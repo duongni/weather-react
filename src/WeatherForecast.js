d2eea43e2a6c4f076cf90b96913af9f98bf9fa9b
duongni <133175915+duongni@users.noreply.github.com>
2023-07-05 11:01:09 -0400

    Added Style for WeatherForecast Component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,28 +1,41 @@
 import React from "react";
-import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
   return (
-    <div>
-      <div className="WeatherForecast">
+    <div className="WeatherForecast">
+      <div className="ForecastDate">
         <p>Date</p>
         <i className={`wi wi-owm-${props.data.icon} mb-3`}></i>
         <p>
           80° <span>60°</span>
         </p>
       </div>
-      <div className="WeatherForecast">
+      <div className="ForecastDate">
         <p>Date</p>
         <i className={`wi wi-owm-${props.data.icon} mb-3`}></i>
         <p>
-          80° <span>60°</span>
+          80° |<span> 60°</span>
         </p>
       </div>
-      <div className="WeatherForecast">
+      <div className="ForecastDate">
         <p>Date</p>
         <i className={`wi wi-owm-${props.data.icon} mb-3`}></i>
         <p>
-          80° <span>60°</span>
+          80° |<span> 60°</span>
+        </p>
+      </div>
+      <div className="ForecastDate">
+        <p>Date</p>
+        <i className={`wi wi-owm-${props.data.icon} mb-3`}></i>
+        <p>
+          80° |<span> 60°</span>
+        </p>
+      </div>
+      <div className="ForecastDate">
+        <p>Date</p>
+        <i className={`wi wi-owm-${props.data.icon} mb-3`}></i>
+        <p>
+          80° |<span> 60°</span>
         </p>
       </div>
     </div>
